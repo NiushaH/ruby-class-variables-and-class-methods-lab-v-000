1d9953d3bc674bee9ed0bc67023dd2cac41f7583
Niusha H <niusha.hadziomerovic@gmail.com>
2018-08-18 04:24:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,12 +37,12 @@ attr_accessor :name, :artist, :genre
       genre_count = Hash.new(0)
       @@genres.each do |genre|
         genre_count[genre] += 1
-        end  
+        end
       genre_count
     end
   
     def self.artist_count
-      artist_count = Hash.new(0)
+      artist_count = {}(0)
       @@artists.each do |artist|
         artist_count[artist] += 1
         end  
